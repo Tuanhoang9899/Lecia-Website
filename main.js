bbaa3d61728628b0743d1beb3719a906dc2526e2
thanhnam110420 <89840094+thanhnam110420@users.noreply.github.com>
2023-06-16 15:19:33 +0700

    update code

--- main.js
+++ main.js
@@ -1,5 +1,58 @@
 $( document ).ready(function () {
-   
-
-
+    $(".nav-bar").click(function (){
+        $(".modal_menu-mobile").show();
+        $(".modal_menu-mobile").addClass("show-mobile");
+        $("body").css("overflow", "hidden");
+    });
+    $(".menu-mobile").click(function(event) {
+        event.stopPropagation();
+    });
+    $(".modal_menu-mobile").click(function() {
+        $(this).removeClass("show-mobile");
+        $("body").css("overflow", "visible");
+        $(".overlay").hide();
+    });
+    $(".nav-bar").click(function() {
+        $(".overlay").fadeIn();
+    });
+    $(".overlay").click(function() {
+        $(this).fadeOut();
+    });
+    if($(window).width() < 991) {
+        $(".headtitle").click(function () {
+            $(this).toggleClass("minus");
+            var sibling = $(this).next();
+            sibling.slideToggle();
+        });
+    }
+    if ($(".content-services .services-item").length) {
+        $(".content-services").owlCarousel({
+            items: 2,
+            nav: false,
+            dots: false,
+            autoplay:false,
+            margin:12,
+            loop:false,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                320: {
+                    items: 1,
+                    nav: true,
+                },
+                480: {
+                    items: 1,
+                    nav: true,
+                },
+                768: {
+                    items: 1,
+                    nav: true,
+                },
+                992: {
+                    items: 1.5
+                }
+            },
+        });
+    }
 });
